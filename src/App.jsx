eb97f7108fd77c074647412a2e85f6dbe5a79442
vivek954sahu <vivek954sahu@gmail.com>
2026-02-25 13:35:25 +0530

    App.jsx Completed.

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,17 @@
+import Footer from "./components/footer/Footer";
+import Header from "./components/header/Header";
+import Main from "./components/main/Main";
+import { GlobalStyle } from "./styles/globalStyles";
 
 function App() {
- return (
-  <>
-  </>
- )
+  return (
+    <>
+    <GlobalStyle />
+    <Header />
+    <Main />
+    <Footer />
+    </>
+  );
 }
 
-export default App
+export default App;
